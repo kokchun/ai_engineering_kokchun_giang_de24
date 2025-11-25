1617880fc44770db88a519b6a6641bcd164c8599
kokchun <kokchungiang@gmail.com>
2025-11-25 13:14:54 +0100

    path parameter

--- 06_fastapi_crud/api.py
+++ 06_fastapi_crud/api.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
-from data_processing import library_data
+from data_processing import library_data, Book
 
 library = library_data("library.json")
-books = library.books
+
+books: list[Book] = library.books
 
 app = FastAPI()
 
 
-
 @app.get("/books")
 async def read_books():
-    return books 
+    return books
+
+# path parameter
+@app.get("/book/{id}")
+async def read_book_by_id(id: int):
+    return [book for book in books if book.id == id][0]
+
+
+@app.post("/books/create_book")
+async def create_book(book_request: Book):
+    ...
