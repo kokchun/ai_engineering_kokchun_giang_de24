1ac293d79137e27aa162ae5c7a736a08dc9dc298
kokchun <kokchungiang@gmail.com>
2025-11-27 11:27:46 +0100

    connect fastapi with duckdb

--- 07_b_pydanticai_fastapi/movies_api.py
+++ 07_b_pydanticai_fastapi/movies_api.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
 from agents import movie_agent
 from data_models import Prompt
+from utils import query_duckdb
 
 app = FastAPI()
 
 
 @app.get("/movies")
 async def read_movies():
-    pass 
+    movies = query_duckdb("FROM movies;")
+    return movies.to_dict(orient="records")
+
 
 @app.post("/movies/create_movie")
 async def create_movie(query: Prompt):
     result = await movie_agent.run(query.prompt)
-    movie = result.output
+    movie = result.output  # pydantic model
 
-    return movie 
+    query_duckdb(
+        "INSERT INTO movies VALUES (?,?,?,?);",
+        parameters=[movie.title, movie.year, movie.genre, movie.rating],
+    )
 
+    return movie
