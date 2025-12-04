5f82892adbe15be27013f55d27da957a27caa89a
kokchun <kokchungiang@gmail.com>
2025-12-04 10:18:27 +0100

    new files

---
+++ 10_rag/backend/data_models.py
@@ -0,0 +1,17 @@
+from lancedb.pydantic import LanceModel, Vector
+from lancedb.embeddings import get_registry
+from dotenv import load_dotenv
+from pydantic import Field
+
+load_dotenv()
+
+embedding_model = get_registry().get("gemini-text").create(name="gemini-embedding-001")
+
+EMBEDDING_DIM = 3072
+
+class Article(LanceModel):
+    doc_id: str 
+    filepath: str 
+    filename: str = Field(description="stem of the file, without suffix")
+    content: str = embedding_model.SourceField()
+    embedding: Vector(EMBEDDING_DIM) = embedding_model.VectorField()
