a12cbb95e985564f05fc738cd916dbc809e88b02
kokchun <kokchungiang@gmail.com>
2025-12-04 10:44:22 +0100

    more data models for rag

--- 10_rag/backend/data_models.py
+++ 10_rag/backend/data_models.py
@@ -1,7 +1,7 @@
 from lancedb.pydantic import LanceModel, Vector
 from lancedb.embeddings import get_registry
 from dotenv import load_dotenv
-from pydantic import Field
+from pydantic import Field, BaseModel
 
 load_dotenv()
 
@@ -16,3 +16,11 @@ class Article(LanceModel):
     filename: str = Field(description="stem of the file, without suffix")
     content: str = embedding_model.SourceField()
     embedding: Vector(EMBEDDING_DIM) = embedding_model.VectorField()
+
+class Prompt(BaseModel):
+    prompt: str = Field(description="prompt from user")
+
+class RagResponse(BaseModel):
+    filename: str = Field(description="filename of retrieved file without suffix")
+    filepath: str = Field(description="absolute path to retrieved file")
+    answer: str = Field(description="answer based on retrieved file")
