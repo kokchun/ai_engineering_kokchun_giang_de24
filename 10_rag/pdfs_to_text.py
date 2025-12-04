55ce9309b41a5311092352686b09ed14ed64d453
kokchun <kokchungiang@gmail.com>
2025-12-04 09:47:22 +0100

    export txt from pdfs

--- 10_rag/pdfs_to_text.py
+++ 10_rag/pdfs_to_text.py
@@ -1,5 +1,5 @@
 from pypdf import PdfReader
-
+from backend.constants import DATA_PATH
 
 def extract_text_from_pdf(path) -> str:
     reader = PdfReader(path)
@@ -10,3 +10,18 @@ def extract_text_from_pdf(path) -> str:
         all_text += page.extract_text() + "\n"
 
     return all_text
+
+
+def export_text_to_txt(text, export_path):
+    with open(export_path, "w", encoding="utf-8") as file:
+        file.write(text)
+
+
+if __name__ == "__main__":
+    # print(tuple(DATA_PATH.glob("*.pdf")))
+    
+    for pdf_path in DATA_PATH.glob("*.pdf"):
+        pdf_text = extract_text_from_pdf(pdf_path)
+
+        filename = f"{pdf_path.stem.casefold()}.txt"
+        export_text_to_txt(pdf_text, export_path= DATA_PATH / filename)
